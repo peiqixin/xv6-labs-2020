1a65cd23f775d37d9b6ab437f6dabe17444ba4bd
qxp <873890482@qq.com>
2021-05-08 00:57:30 +0800

    pte printout: OK

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,41 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// recursive 
+void vmprint_helper(pagetable_t pagetable, int level) {
+  if (level > 2) return;
+  for (int i = 0; i < 512; ++i) {
+    pte_t* pte = &pagetable[i];
+    if (*pte & PTE_V) {
+      pagetable_t cur = (pagetable_t)PTE2PA(*pte);
+      for (int j = 0; j <= level; ++j) {
+        printf("..");
+        if (j != level) printf(" ");
+      }
+      printf("%d: pte %p pa %p\n", i, *pte, cur);
+      vmprint_helper(cur, level + 1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 0);
+}
+
+// not recursive 
+// void vmprint(pagetable_t pagetable) {
+//   for (int level = 2; level > 0; level--) {
+//     pte_t* pte = &pagetable[PX(level, q)];
+//     if (*pte & PTE_V) {
+//       pagetable = (pagetable_t)PTE2PA(*pte);
+//     }
+//     else {
+//       if (!alloc || (pagetable = (pde_t*)kalloc()) == 0)
+//         return 0;
+//       memset(pagetable, 0, PGSIZE);
+//       *pte = PA2PTE(pagetable) | PTE_V;
+//     }
+//   }
+// }
